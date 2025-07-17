01ebaadcdb2a3011f516343e85ba84e398f3bb01
Boombastick4 <boombastick4@protonmail.com>
2025-07-17 14:33:34 +0200

    added calculator.rs

---
+++ simple_calculator/src/calculator.rs
@@ -0,0 +1,10 @@
+fn add(a: i32, b: i32){return a + b; }
+
+fn sub(a: i32, b: i32){return a - b; }
+
+fn mul(a: i32, b: i32){return a * b; }
+
+fn div(a: i32, b: i32){
+    if(b == 0) return Err("Cannot difde by 0!!!"); 
+
+    else return a - b; }
