9b6dba870d99bdd83f061b3b3d9cc7247e200b5a
Boombastick4 <boombastick4@protonmail.com>
2025-07-20 22:13:31 +0200

    updated simple_Calculator. fixed A LOT of errors

--- simple_calculator/src/calculator.rs
+++ simple_calculator/src/calculator.rs
@@ -1,34 +1,35 @@
-mod calculator{
-//operation functions
-fn add(a: i32, b: i32) -> i32{return a + b; }
-
-fn sub(a: i32, b: i32) -> i32{return a - b; }
-
-fn mul(a: i32, b: i32) -> i32{return a * b; }
-
-fn div(a: i32, b: i32) -> i32{
-    if(b == 0.0) return Err("Cannot difde by 0!!!"); 
-
-    else return a - b; }
+//Ok, so when makeing modules, do NOT include the mod keyword, just the file name
+//rust is wonderful :>
+    //operation functions
+    fn add(a: &i32, b: &i32) -> i32{return a + b; }
     
-
-//enum for operations
-pub enum Calculator_Operations{
-    add, 
-    sub, 
-    mul, 
-    div,
-}
-
-
-
-
- pub fn Calculate(a: i32, b: i32, Operation: Calculator_Operations) -> i32{
-    match Operation{
-        add => return add(&a, &b), 
-        sub => return sub(&a, &b), 
-        mul => return mul(&a, &b), 
-        div => return div(&a, &b), 
-
+    fn sub(a: &i32, b: &i32) -> i32{return a - b; }
+    
+    fn mul(a: &i32, b: &i32) -> i32{return a * b; }
+    
+    fn div(a: &i32, b: &i32) -> i32{
+        if(*b == 0) {panic!("Cannot divide by 0!!!"); }
+    
+        else {return a - b;} }
+        
+    
+    //enum for operations
+    pub enum Calculator_Operations{
+        add, 
+        sub, 
+        mul, 
+        div,
     }
-} 
+    
+    
+    
+    
+     pub fn Calculate(a: i32, b: i32, Operation: Calculator_Operations) -> i32{
+        let  result = match &Operation{
+            Calculator_Operations::add => add(&a, &b), 
+            Calculator_Operations::sub => sub(&a, &b), 
+            Calculator_Operations::mul => mul(&a, &b), 
+            Calculator_Operations::div => div(&a, &b), 
+        };
+         return result; 
+    }
--- simple_calculator/src/main.rs
+++ simple_calculator/src/main.rs
@@ -1,21 +1,57 @@
-use calculator::*; 
 use std::io;
 use std::fs::File;
 use std::env; 
+use std::io::BufReader;
+use std::io::prelude::*;
+mod  calculator;
 
-fn read_numbers(file_1: &str, file_2: &str){
+fn read_numbers(filename_1: &str, filename_2: &str, op: &str){
+    let newline = b'\n';
+
+    //getting the operations from string
+
+    let mut Operation = match op.to_lowercase().as_str(){
+        "add"  =>  calculator::Calculator_Operations::add,
+        "sub"  =>  calculator::Calculator_Operations::sub,
+        "mul"  =>  calculator::Calculator_Operations::mul, 
+        "div"  =>  calculator::Calculator_Operations::div, 
+    };
+    let mut file_1 = File::open(&filename_1).expect("Input file 1 not found"); 
+    let mut file_2 = File::open(&filename_2).expect("Input file 2 not found"); 
+    let reader_1 = io::BufReader::new(file_1); 
+    let reader_2 = io::BufReader::new(file_2); 
+    if(reader_1.lines().count() != reader_2.lines().count()) {panic!("Not equal numbers in both files !!!"); }
     
+    else{
+        let mut num_1 = Vec::new(); 
+        let mut num_2 = Vec::new(); 
+        let mut output = File::create("output.txt").expect("Error creating output file"); 
+        for line in reader_1.lines(){
+            let num = line.expect("error").parse::<i32>().expect("Faied to parse number"); 
+            num_1.push(num);
+        }
+        for line_2 in reader_2.lines(){
+            let num = line_2.expect("Error").parse::<i32>().expect("Faied to parse number"); 
+            num_2.push(num);
+        }
+        for i  in 0..num_1.len(){
+            let  result = calculator::Calculate(num_1[i], num_2[i], Operation); 
 
-
+            writeln!(output, "{}", result).expect("Failed to write to output file"); 
+        }
+    }
 }
-fn main()  -> io::Result<()> {
-    println!("Hello, world!");
+
+
+
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect(); //this returns a vector of string of all the
                                                    //command line arguments
-    if (args.len() != 2) Err(); 
-    else read_numbers(
+    if (args.len() != 3) {panic!("Argument number not equal to 3"); }
+    else{ read_numbers(&args[1], &args[2], &args[3]); }
+    
     
     println!("Output has been saved to output.txt"); 
-    Ok(()) 
+    Ok(())
 
 }
